4589be31fe7c62d73e00d80fc853b4d0bfae985c
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-09-08 04:49:12 +0100

    recieving http request

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,8 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, io::Read};
 
 pub struct Server {
     addr: String
-}
+}  
 
 impl Server {
     pub fn new(addr: String) -> Self {
@@ -12,9 +12,24 @@ impl Server {
     }
 
     pub fn run(self){
-         println!("listening on port {}", self.addr);
+        println!("listening on port {}", self.addr);
 
-         let listener = TcpListener::bind(&self.addr).unwrap();
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read( &mut  buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("failed to establish a connectionon: {}", e),
+                    }
+                } 
+                Err(e) => println!("failed to establish a connection: {}", e)
+            }
+        }
     }
 }
 
