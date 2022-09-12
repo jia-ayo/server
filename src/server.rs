d7b353befcd7e1b20042c2914a9e785ca09765ce
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-09-12 01:25:34 +0100

    Add Error handling

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,11 @@
-use std::{net::TcpListener, io::Read};
+use std::{
+     net::TcpListener,
+     io::Read, 
+     convert::TryFrom,
+     convert::TryInto
+    };
+
+use crate::http::Request;
 
 pub struct Server {
     addr: String
@@ -23,6 +30,12 @@ impl Server {
                     match stream.read( &mut  buffer) {
                         Ok(_) => {
                             println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]){
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e )
+                            };
+                            
                         }
                         Err(e) => println!("failed to establish a connectionon: {}", e),
                     }
